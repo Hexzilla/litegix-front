94090f1487910ff48061fdc4ccd884741cdfba92
rightvalue <bluebird6922@gmail.com>
2021-06-24 11:42:05 +0300

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/metronic/vue/demo1/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   configureWebpack: {
     resolve: {
       alias: {

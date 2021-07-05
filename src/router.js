9555375500eaab4cd465e9e902bf78349b15e2b7
rightvalue <bluebird6922@gmail.com>
2021-07-05 10:49:21 +0300

    delete profile pages

--- src/router.js
+++ src/router.js
@@ -720,32 +720,6 @@ export default new Router({
           ]
         },
         {
-          path: "/custom-wizard",
-          name: "wizard",
-          component: () => import("@/view/pages/wizard/Wizard.vue"),
-          children: [{
-              path: "wizard-1",
-              name: "wizard-1",
-              component: () => import("@/view/pages/wizard/Wizard-1.vue")
-            },
-            {
-              path: "wizard-2",
-              name: "wizard-2",
-              component: () => import("@/view/pages/wizard/Wizard-2.vue")
-            },
-            {
-              path: "wizard-3",
-              name: "wizard-3",
-              component: () => import("@/view/pages/wizard/Wizard-3.vue")
-            },
-            {
-              path: "wizard-4",
-              name: "wizard-4",
-              component: () => import("@/view/pages/wizard/Wizard-4.vue")
-            }
-          ]
-        },
-        {
           path: "/custom-plugins",
           name: "plugins",
           component: () => import("@/view/pages/plugins/Plugins.vue"),
@@ -758,33 +732,6 @@ export default new Router({
               path: "treeselect",
               name: "treeselect",
               component: () => import("@/view/pages/plugins/Treeselect.vue")
-            }
-          ]
-        },
-        {
-          path: "/profile",
-          name: "profile",
-          component: () => import("@/view/pages/profile/Profile.vue"),
-          children: [
-            {
-              path: "profile-1",
-              name: "profile-1",
-              component: () => import("@/view/pages/profile/Profile-1.vue")
-            },
-            {
-              path: "profile-2",
-              name: "profile-2",
-              component: () => import("@/view/pages/profile/Profile-2.vue")
-            },
-            {
-              path: "profile-3",
-              name: "profile-3",
-              component: () => import("@/view/pages/profile/Profile-3.vue")
-            },
-            {
-              path: "profile-4",
-              name: "profile-4",
-              component: () => import("@/view/pages/profile/Profile-4.vue")
             }
           ]
         }

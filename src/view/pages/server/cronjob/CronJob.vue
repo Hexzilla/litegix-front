f2f72392f6d43486355840be039cc914dd632136
ShkuratovU <ShkuratovMong@mail.ru>
2021-07-06 11:36:07 +0900

    CronJob layout

--- src/view/pages/server/cronjob/CronJob.vue
+++ src/view/pages/server/cronjob/CronJob.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card card-custom gutter-b">
-    <div class="card-header border-0 py-5">
+    <div class="card-header border-0 pt-5 pb-0">
       <h3 class="card-title align-items-start flex-column">
         <span class="card-label font-weight-bolder text-dark">Cron Job</span>
       </h3>
@@ -10,32 +10,32 @@
           placeholder="Search..."
           class="form-control input-lg w-200px mr-5"
         />
-        
+
         <b-link to="cronjob/create">
-              <a class="btn btn-success">Create</a>
-            </b-link>
+          <a class="btn btn-success">Create</a>
+        </b-link>
       </div>
-      <span
-        class="text-muted mt-3 font-weight-bold font-size-sm"
-        data-nsfw-filter-status="swf"
-        >Rebuilding Cron Job will delete all Cron Job configs that was generated
-        by RunCloud. We will add it back to match your configuration
-        below.</span
-      >
     </div>
     <div class="card-body py-0">
-       <b-table
-          :items="items"
-          :fields="fields"
-          show-empty
-          empty-text="You don't have any cron jobs configured yet."
-        >
-          <template #cell(view)>
-            <span class="svg-icon svg-icon-primary">
+      <div
+        class="text-muted mt-3 mb-5 font-weight-bold font-size-sm"
+        data-nsfw-filter-status="swf"
+      >
+        Rebuilding Cron Job will delete all Cron Job configs that was generated
+        by RunCloud. We will add it back to match your configuration below.
+      </div>
+      <b-table
+        :items="items"
+        :fields="fields"
+        show-empty
+        empty-text="You don't have any cron jobs configured yet."
+      >
+        <template #cell(view)>
+          <span class="svg-icon svg-icon-primary">
             <inline-svg src="media/svg/icons/Design/Layers.svg" />
           </span>
-          </template>
-        </b-table>
+        </template>
+      </b-table>
     </div>
   </div>
 </template>

4ddd1dea7a11b01634d4fd1a650c409a448a0668
goldencat-tom <bluebird6922@gmail.com>
2021-08-09 12:35:56 -0700

    add SSH key vault

--- src/view/pages/settings/SSHKeyVault.vue
+++ src/view/pages/settings/SSHKeyVault.vue
@@ -1,69 +1,62 @@
 <template>
-    <!--begin::Advance Table Widget 10-->
-    <v-card class="card-body">
-      <!--begin::Header-->
-      <v-card-title>
-          <h1>SSH KEY Vault</h1>
-      </v-card-title>
-      <p class="text-muted font-size-sm mt-1">
-        This is where you can add your SSH Key to make it easy for you to
-            add the key to your server.
-      </p>
-      <!--end::Header-->
-      <!--begin::Body-->
-      <div class="card-body">
-        <v-data-table :headers="headers" :items="SSHKeys" sort-by="calories">
-          <template v-slot:top>
-            <v-toolbar flat>
-              <v-spacer></v-spacer>
-              <v-text-field 
-                outlined 
-                dense 
-                label="Search" 
-                hide-details=""
-                class="searchSSHKey"
-              ></v-text-field>
-              <v-btn 
-              color="primary" 
-              @click="dialogDelete = true"
-                >Add SSH Key</v-btn
-              >
-              <v-dialog v-model="dialogDelete" max-width="500px">
-                <v-card>
-                  <v-card-title class="text-h5"
-                    >Are you sure you want to delete this item?</v-card-title
-                  >
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                      color="blue darken-1"
-                      text
-                      @click="dialogDelete = false"
-                      >Cancel</v-btn
-                    >
-                    <v-btn
-                      color="blue darken-1"
-                      text
-                      @click="dialogDelete = false"
-                      >OK</v-btn
-                    >
-                    <v-spacer></v-spacer>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-            </v-toolbar>
-          </template>
-          <template v-slot:item.actions="{ item }">
-            <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-          </template>
-        </v-data-table>
+  <div class="card card-custom">
+    <div class="card-header py-3">
+      <div class="card-title align-items-start flex-column">
+        <h3 class="card-label font-weight-bolder text-dark">
+          SSH KEY Vault
+        </h3>
+        <span class="text-muted font-weight-bold font-size-sm mt-1"
+          >Manage SSH key vault</span
+        >
       </div>
-    </v-card>
+      <div class="card-toolbar">
+        <button
+          type="reset"
+          class="btn btn-success mr-2"
+          @click="add()"
+          ref="kt_save_changes"
+        >
+          Add SSH Key
+        </button>
+      </div>
+    </div>
+    <div class="card-body">
+      <v-data-table :headers="headers" :items="SSHKeys" sort-by="calories">
+        <template v-slot:top>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5"
+                >Are you sure you want to delete this item?</v-card-title
+              >
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="dialogDelete = false"
+                  >Cancel</v-btn
+                >
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="dialogDelete = false"
+                  >OK</v-btn
+                >
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+        </template>
+      </v-data-table>
+    </div>
+  </div>
 </template>
 <style scoped>
 .searchSSHKey {
   max-width: 200px;
-  margin-right: 40px;
 }
 </style>
 <script>
@@ -76,12 +69,12 @@ export default {
         text: "Label",
         align: "start",
         sortable: false,
-        value: "label"
+        value: "label",
       },
       { text: "Public Key", value: "key" },
-      { text: "Delete", value: "actions", sortable: false }
+      { text: "Delete", value: "actions", sortable: false },
     ],
-    SSHKeys: []
+    SSHKeys: [],
   }),
 
   created() {
@@ -90,7 +83,7 @@ export default {
   mounted() {
     this.$store.dispatch(SET_BREADCRUMB, [
       { title: "Settings", route: "profile" },
-      { title: "SSH KEY Vault" }
+      { title: "SSH KEY Vault" },
     ]);
   },
 
@@ -99,15 +92,15 @@ export default {
       this.SSHKeys = [
         {
           label: "Frozen Yogurt",
-          key: 159
-        }
+          key: 159,
+        },
       ];
     },
 
     deleteItem() {
       //console.log(item);
       this.dialogDelete = true;
-    }
-  }
+    },
+  },
 };
 </script>

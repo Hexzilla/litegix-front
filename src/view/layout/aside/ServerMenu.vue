e7f175b362213d25e169c0b11c6d3f11a5da8b88
goldencat-tom <bluebird6922@gmail.com>
2021-08-27 13:04:40 +0300

    update server sub menu

--- src/view/layout/aside/ServerMenu.vue
+++ src/view/layout/aside/ServerMenu.vue
@@ -1,383 +1,120 @@
 <template>
-  <div
-    class="navi navi-bold navi-hover navi-active navi-link-rounded"
-    role="tablist"
-  >
-    <router-link
-      :to="`/servers/` + serverId + `/server/summary`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          style="cursor: pointer"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/Design/Layers.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">Summary</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/health`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          style="cursor: pointer"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/Code/Compiling.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">Server Health</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/application`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          style="cursor: pointer"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/Communication/Shield-user.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">Web Application</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/database`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          style="cursor: pointer"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/Communication/RSS.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">Database</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/user`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          style="cursor: pointer"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg
-                src="media/svg/icons/Communication/Incoming-call.svg"
-              />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">System User</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/sshkey`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          style="cursor: pointer"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/Communication/Thumbtack.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">SSH Key</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/deployment`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          style="cursor: pointer"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/Communication/Reply-all.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">Deployment Key</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/phpcli`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          style="cursor: pointer"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/Communication/Reply-all.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">PHP CLI</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/cronjob`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          style="cursor: pointer"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/Communication/Safe-chat.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">Cron Job</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/supervisor`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          style="cursor: pointer"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/Communication/Sending.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">Supervisor</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/notification`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          style="cursor: pointer"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/General/User.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">Notifications</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/services`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          style="cursor: pointer"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/Communication/Mail-at.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">Services</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/security`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          style="cursor: pointer"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/Communication/Group.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">Security</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/settings`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          style="cursor: pointer"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/Communication/Share.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">Settings</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/activitylog`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          style="cursor: pointer"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/Communication/Mail-opened.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">Activity Log</span>
-        </a>
-      </div>
-    </router-link>
-
-    <router-link
-      :to="`/servers/` + serverId + `/server/delete`"
-      v-slot="{ href, navigate, isActive, isExactActive }"
-    >
-      <div class="navi-item mb-2">
-        <a
-          class="navi-link py-4"
-          style="cursor: pointer"
-          :class="[isActive && 'active', isExactActive && 'active']"
-          :href="href"
-          @click="navigate"
-          aria-selected="false"
-        >
-          <span class="navi-icon mr-2">
-            <span class="svg-icon">
-              <inline-svg src="media/svg/icons/Communication/Delete-user.svg" />
-            </span>
-          </span>
-          <span class="navi-text font-size-lg">Delete Server</span>
-        </a>
-      </div>
-    </router-link>
+  <div class="navi navi-bold navi-hover navi-active row p-5" role="tablist">
+    <template v-for="(item, i) in menuItems">
+      <router-link
+        v-bind:key="i"
+        :to="`/servers/` + serverId + item.link"
+        v-slot="{ href, navigate, isActive, isExactActive }"
+      >
+        <div class="navi-item col-sm-6 border h-md-100px" :class="item.class">
+          <a
+            class="navi-link navi-rect h-100"
+            :class="[isActive && 'active', isExactActive && 'active']"
+            :href="href"
+            @click="navigate"
+            style="cursor: pointer"
+            aria-selected="false"
+          >
+            <div class="navi-content">
+              <div class="navi-icon">
+                <span class="svg-icon">
+                  <inline-svg :src="item.icon" />
+                </span>
+              </div>
+              <div class="navi-text font-size-lg">{{ item.title }}</div>
+            </div>
+          </a>
+        </div>
+      </router-link>
+    </template>
   </div>
 </template>
-
-
 <script>
 export default {
+  name: "ServerMenu",
   props: ["serverId"],
+  data() {
+    return {
+      menuItems: [
+        {
+          title: "Summary",
+          link: "/server/summary",
+          icon: "media/svg/icons/Design/Layers.svg",
+          class: "border-radius-top-left"
+        },
+        {
+          title: "Server Health",
+          link: "/server/health",
+          icon: "media/svg/icons/Code/Compiling.svg",
+          class: "border-radius-top-right"
+        },
+        {
+          title: "Web Application",
+          link: "/server/application",
+          icon: "media/svg/icons/Communication/Mail-notification.svg"
+        },
+        {
+          title: "Database",
+          link: "/server/database",
+          icon: "media/svg/icons/Communication/Reply-all.svg"
+        },
+        {
+          title: "System User",
+          link: "/server/user",
+          icon: "media/svg/icons/Communication/Mail-opened.svg"
+        },
+        {
+          title: "SSH Key",
+          link: "/server/sshkey",
+          icon: "media/svg/icons/Communication/Safe-chat.svg"
+        },
+        {
+          title: "Deployment Key",
+          link: "/server/deployment",
+          icon: "media/svg/icons/Communication/Sending.svg"
+        },
+        {
+          title: "PHP CLI",
+          link: "/server/phpcli",
+          icon: "media/svg/icons/Communication/Thumbtack.svg"
+        },
+        {
+          title: "Notifications",
+          link: "/server/notification",
+          icon: "media/svg/icons/Communication/Sending.svg"
+        },
+        {
+          title: "Services",
+          link: "/server/services",
+          icon: "media/svg/icons/Communication/Thumbtack.svg"
+        },
+        {
+          title: "Cron Job",
+          link: "/server/cronjob",
+          icon: "media/svg/icons/Communication/Sending.svg"
+        },
+        {
+          title: "Supervisor",
+          link: "/server/supervisor",
+          icon: "media/svg/icons/Communication/Thumbtack.svg"
+        },
+        {
+          title: "Activity Log",
+          link: "/server/activitylog",
+          icon: "media/svg/icons/Communication/Mail-at.svg",
+          class: "border-radius-bottom-left"
+        },
+        {
+          title: "Settings",
+          link: "/server/settings",
+          icon: "media/svg/icons/Communication/Share.svg",
+          class: "border-radius-bottom-right"
+        }
+      ]
+    };
+  }
 };
 </script>
+<style lang="scss">
+@import "@/assets/sass/pages/aside.scss";
+</style>
--- src/view/layout/aside/SubAside.vue
+++ src/view/layout/aside/SubAside.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- begin:: Aside -->
   <v-app>
     <div
       class="flex-row-auto offcanvas-mobile w-300px w-xl-350px"
@@ -8,48 +7,53 @@
     >
       <v-card>
         <div class="card-body pt-14">
-          <div class="text-center mb-10">
-            <div class="symbol symbol-60 symbol-circle symbol-xl-90">
-              <div
-                class="symbol-label"
-                :style="{
-                  backgroundImage: `url(${currentUserPhoto})`,
-                }"
-              ></div>
-              <i class="symbol-badge symbol-badge-bottom bg-success"></i>
+          <div v-if="n_path == 'settings'">
+            <div class="text-center mb-10">
+              <div class="symbol symbol-60 symbol-circle symbol-xl-90">
+                <div
+                  class="symbol-label"
+                  :style="{
+                    backgroundImage: `url(${currentUserPhoto})`
+                  }"
+                ></div>
+                <i class="symbol-badge symbol-badge-bottom bg-success"></i>
+              </div>
+
+              <h4 class="font-weight-bold my-2">James Jones</h4>
+              <div class="text-muted mb-2">Application Developer</div>
+              <span
+                class="
+                  label label-light-warning label-inline
+                  font-weight-bold
+                  label-lg
+                "
+                >Active</span
+              >
             </div>
 
-            <h4 class="font-weight-bold my-2">James Jones</h4>
-            <div class="text-muted mb-2">Application Developer</div>
-            <span
-              class="
-                label label-light-warning label-inline
-                font-weight-bold
-                label-lg
-              "
-              >Active</span
-            >
-          </div>
-          <!--end::User-->
+            <div class="mb-10 text-center">
+              <a
+                href="#"
+                class="btn btn-icon btn-circle btn-light-facebook mr-2"
+              >
+                <i class="socicon-facebook"></i>
+              </a>
+              <a
+                href="#"
+                class="btn btn-icon btn-circle btn-light-twitter mr-2"
+              >
+                <i class="socicon-twitter"></i>
+              </a>
+              <a href="#" class="btn btn-icon btn-circle btn-light-google">
+                <i class="socicon-google"></i>
+              </a>
+            </div>
 
-          <!--begin::Contact-->
-          <div class="mb-10 text-center">
-            <a href="#" class="btn btn-icon btn-circle btn-light-facebook mr-2">
-              <i class="socicon-facebook"></i>
-            </a>
-            <a href="#" class="btn btn-icon btn-circle btn-light-twitter mr-2">
-              <i class="socicon-twitter"></i>
-            </a>
-            <a href="#" class="btn btn-icon btn-circle btn-light-google">
-              <i class="socicon-google"></i>
-            </a>
+            <KTSettingsSubMenu></KTSettingsSubMenu>
           </div>
-          <!--end::Contact-->
-          <KTSettingsSubMenu v-if="n_path == 'settings'"></KTSettingsSubMenu>
-          <KTServerSubMenu
-            :serverId="serverId"
-            v-if="n_path == 'server'"
-          ></KTServerSubMenu>
+          <div v-if="n_path == 'server'">
+            <KTServerSubMenu :serverId="serverId"></KTServerSubMenu>
+          </div>
         </div>
       </v-card>
     </div>
@@ -69,12 +73,12 @@ export default {
   data() {
     return {
       insideTm: 0,
-      outsideTm: 0,
+      outsideTm: 0
     };
   },
   components: {
     KTServerSubMenu,
-    KTSettingsSubMenu,
+    KTSettingsSubMenu
   },
   mounted() {
     this.serverId = this.$parent.serverId;
@@ -84,7 +88,7 @@ export default {
       // Init Aside
       KTLayoutAside.init(asideRef);
 
-      asideRef.querySelectorAll(".navi-link").forEach((item) => {
+      asideRef.querySelectorAll(".navi-link").forEach(item => {
         item.addEventListener("click", () => {
           KTLayoutAside.getOffcanvas().hide();
         });
@@ -95,7 +99,7 @@ export default {
     });
   },
   computed: {
-    ...mapGetters(["currentUserPhoto"]),
-  },
+    ...mapGetters(["currentUserPhoto"])
+  }
 };
 </script>

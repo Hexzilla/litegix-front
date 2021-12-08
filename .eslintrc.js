04b8af546cd3867f429c4f9c03ae7b18a751f18b
goldencat-tom <bluebird6922@gmail.com>
2021-12-08 19:58:57 +0900

    fix ellintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,9 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": process.env.NODE_ENV === "production" ? "off" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "off" : "off",
+    "no-unused-vars": process.env.NODE_ENV === "production" ? "off" : "off",
     "max-len": ["error", { code: 360 }]
   },
   parserOptions: {

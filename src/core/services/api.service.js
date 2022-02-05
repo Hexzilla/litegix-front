727d144405f9b0f76a8a2d8f97e453fa35da34e5
Eric Thomas <bluebird6922@gmail.com>
2022-02-05 15:49:10 +0900

    change backend server

--- src/core/services/api.service.js
+++ src/core/services/api.service.js
@@ -11,7 +11,7 @@ const ApiService = {
     Vue.use(VueAxios, axios);
     Vue.axios.defaults.baseURL =
       process.env.NODE_ENV === "production"
-        ? "http://5.161.73.6/" //"https://litegix-server.herokuapp.com/"
+        ? "https://litegix.herokuapp.com/" //"http://5.161.73.6/" //"https://litegix-server.herokuapp.com/"
         : "http://localhost:3000";
   },
 
